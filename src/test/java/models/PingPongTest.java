8141e09df87041736b9bb628fb11db61de41cd4d
Eric Wafula <eric.wafula@student.moringaschool.com>
2021-08-02 22:57:59 +0300

    Initial commit

--- src/test/java/models/PingPongTest.java
+++ src/test/java/models/PingPongTest.java
@@ -8,9 +8,62 @@ import java.util.List;
 public class PingPongTest {
     @Test
     public void runPingPong_countUpToOne_ArrayList() {
-        PingPong testPingPong = new PingPong();
-        List<Object> expectedOutput = new ArrayList<Object>();
+        PingPong newPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
         expectedOutput.add(1);
-        assertEquals(expectedOutput, testPingPong.runPingPong(1));
+        assertEquals(expectedOutput, newPingPong.runPingPong(1));
+    }
+
+    @Test
+    public void runPingPong_countUpToGivenValue_ArrayList() throws Exception {
+        PingPong newPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        assertEquals(expectedOutput, newPingPong.runPingPong(2));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf3_ArrayList() throws Exception {
+        PingPong newPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        assertEquals(expectedOutput, newPingPong.runPingPong(3));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf5_ArrayList() throws Exception {
+        PingPong newPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        assertEquals(expectedOutput, newPingPong.runPingPong(5));
+    }
+
+    @Test
+    public void runPingPong_replaceMultiplesOf3And5_ArrayList() throws Exception {
+        PingPong newPingPong = new PingPong();
+        ArrayList<Object> expectedOutput = new ArrayList<Object>();
+        expectedOutput.add(1);
+        expectedOutput.add(2);
+        expectedOutput.add("ping");
+        expectedOutput.add(4);
+        expectedOutput.add("pong");
+        expectedOutput.add("ping");
+        expectedOutput.add(7);
+        expectedOutput.add(8);
+        expectedOutput.add("ping");
+        expectedOutput.add("pong");
+        expectedOutput.add(11);
+        expectedOutput.add("ping");
+        expectedOutput.add(13);
+        expectedOutput.add(14);
+        expectedOutput.add("pingpong");
+        assertEquals(expectedOutput, newPingPong.runPingPong(15));
     }
 }
